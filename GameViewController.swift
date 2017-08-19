005e8d2564c55a846609a498072b5e60194aa434
Matthew Bourke <Matt@Matthews-MacBook-Pro.local>
2017-08-19 23:30:25 +1000

    Got some comments here now. I understand about 90% of what I wrote. Go me.

--- GameViewController.swift
+++ GameViewController.swift
@@ -16,80 +16,107 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     @IBOutlet weak var addUpdateButton: UIButton!
     @IBOutlet weak var deleteButton: UIButton!
     
+    // UIImagePickerController lets us choose images I guess?
     var imagePicker = UIImagePickerController()
+    
+    // No game yet?
     var game : Game? = nil
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         
+        // Initialises imagePicker
         imagePicker.delegate = self
         
+        // If game does exist, set name and image for said game
         if game != nil {
             gameImage.image = UIImage(data: game!.image! as Data)
             titleTF.text = game?.title
             
+            // Sets the title for the add/update button to update, as a game already exists
             addUpdateButton.setTitle("Update", for: .normal)
+            
+        // If no game exists, hide the delete button
         } else {
             deleteButton.isHidden = true
         }
     }
     
+    // Function for selecting photos from memory
     @IBAction func photosTapped(_ sender: Any) {
-        imagePicker.sourceType = .camera
+        // Sets the source for image as 'photoLibrary'
+        imagePicker.sourceType = .photoLibrary
+        
+        // Photo library appears on screen for selecting photo
         present(imagePicker, animated: true, completion: nil)
     }
     
+    // I have no idea what this is doing lol
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         
         gameImage.image = image
         
+        // All I know is this dismisses the photo library
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
+    // For taking a photo for image - not sure if this works because Apple wants me to pay $100 to test lol //
     @IBAction func cameraTapped(_ sender: Any) {
-        imagePicker.sourceType = .photoLibrary
+        // Sets source of image to camera
+        imagePicker.sourceType = .camera
         
+        // Presents camera
         present(imagePicker, animated: true, completion: nil)
     }
     
+    // Function for adding new game
     @IBAction func addTapped(_ sender: Any) {
-        
+        // If game does exist, title and image are as they were previously saved as
         if game != nil {
             game!.title = titleTF.text
             game!.image = UIImagePNGRepresentation(gameImage.image!) as NSData?
             
+        // If no game exists...
         } else {
+            // Again, not sure what this does
             let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-            
+            // Or this
             let game = Game(context: context)
             
+            // Sets game title and image to input
             game.title = titleTF.text
             game.image = UIImagePNGRepresentation(gameImage.image!) as NSData?
         }
         
         
-        
-        
+        // Saves new game
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
+        // Returns to original VC
         navigationController!.popViewController(animated: true)
-        
-        
     }
     
+    // For deleting saved games
     @IBAction func deleteTapped(_ sender: Any) {
+        // I'm starting to think this is just to access CoreData?
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        // Deletes game
         context.delete(game!)
         
+        // Updates new content
        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
+        // Returns to original VC
         navigationController!.popViewController(animated: true)
     }
     
     
+    // This function is here by default
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- The Collector/ViewController.swift
+++ The Collector/ViewController.swift
@@ -12,55 +12,82 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var gameList: UITableView!
     
+    // Create array of type 'Game' defined in CoreData ?I think?
     var games : [Game] = []
     
+    // viewDidLoad runs first time VC appears
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
+        // Initialisation for tableviews
         gameList.delegate = self
         gameList.dataSource = self
     }
     
+    // Function defines number of rows in tableview
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Number of rows is equal to number of elements in 'games' array
         return games.count
     }
     
+    // Function puts correct information in corresponding cell of tableview
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Define cell as a 'UITableViewCell()
         let cell = UITableViewCell()
+        
+        // Constant game is equal to corresponding item in 'games' array
         let game = games[indexPath.row]
+        
+        // Text in tableview cell is the title of constant 'game'
         cell.textLabel?.text = game.title
+        
+        // Image in tableview cell is the image of 'game'
         cell.imageView?.image = UIImage(data: game.image! as Data)
         
+        // Function returns cell to display information we just put into it
         return cell
     }
     
+    // Function to select the chosen cell in tableview and segue accordingly
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Constant game is has information of corresponding item in 'games' array
         let game = games[indexPath.row]
+        
+        // Performs segue called 'gameSegue' in storyboard. Sends 'game' to next VC
         performSegue(withIdentifier: "gameSegue", sender: game)
     }
     
+    // Sets up segue I guess???
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Defines next VC 'GameViewController' as destination for segue
         let nextVC = segue.destination as! GameViewController
+        
+        // Not sure about this yet, probably intertwines with other VC //
         nextVC.game = sender as? Game
     }
     
-    
+    // Function runs everytime VC appears
     override func viewWillAppear(_ animated: Bool) {
+        // This line is for CoreData, and I'm not exactly sure what's happening
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         do{
+            // Array 'games' fetches data from CoreData
             games = try context.fetch(Game.fetchRequest())
+            
+            // This line reloads the correct data into the tableview
             gameList.reloadData()
         } catch {
-            
+            // Not sure why nothing is in here?
         }
     }
     
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-        }
-        
-        
+    // This function is here by default
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
